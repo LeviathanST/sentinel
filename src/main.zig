0b0bac7efac2542e91d1304361a258ef3197cf41
LeviathanST <phamquochung170505@gmail.com>
2026-05-25 14:20:44 +0700

    feat(cli): add score command

--- src/main.zig
+++ src/main.zig
@@ -2,64 +2,123 @@ const std = @import("std");
 
 const MAX_BIOLOGICAL_RAM = 10;
 
-const Context = struct {
+pub const Idea = struct {
     name: []const u8,
-    complexity: u8,
+    /// How much the comlexity of the idea?
+    complexity: u4, // 0 - 10
+    /// How much technical truth is here? (.e.g., raw code, first principles)
+    signal: u4, // 0 - 10
+    /// How much long-term impact does this have?
+    vision: u4, // 0 - 10
+    /// How much is this just "hype" or "busy work"?
+    noise: u4, // 0 - 10
 };
 
-const ContextList = struct {
-    list: std.ArrayList(Context) = .empty,
+pub const IdeaList = struct {
+    list: std.ArrayList(Idea) = .empty,
     total_complexity: u8 = 0,
 
-    pub fn deinit(self: *ContextList, alloc: std.mem.Allocator) void {
+    pub fn deinit(self: *IdeaList, alloc: std.mem.Allocator) void {
         self.list.deinit(alloc);
     }
 
     pub fn add(
-        self: *ContextList,
+        self: *IdeaList,
         alloc: std.mem.Allocator,
-        ctx: Context,
+        ctx: Idea,
     ) !void {
         try self.list.append(alloc, ctx);
         self.total_complexity += ctx.complexity;
     }
 };
 
-pub fn main(init: std.process.Init) !void {
-    var context_list: ContextList = .{};
-    defer context_list.deinit(init.gpa);
-
-    var arg_iterator = init.minimal.args.iterate();
-    defer arg_iterator.deinit();
-    _ = arg_iterator.next(); // skip the app bin path
-    const command: ?[:0]const u8 = arg_iterator.next();
-
-    if (command != null and std.mem.eql(u8, command.?, "add")) {
-        const name = arg_iterator.next();
-        if (name == null) {
-            std.log.info("ERROR: missing required paramters: `sentinel add <name> <complexity>`", .{});
-            return;
-        }
-        const complexity_str = arg_iterator.next();
-        if (complexity_str == null) {
-            std.log.info("ERROR: missing required paramters: `sentinel add <name> <complexity>`", .{});
-            return;
-        }
-        const complexity = try std.fmt.parseInt(u8, complexity_str.?, 10);
-
-        if (context_list.total_complexity + complexity > MAX_BIOLOGICAL_RAM) {
-            std.log.info("You're overloaded!", .{});
-            return;
-        }
-
-        try context_list.add(init.gpa, .{
-            .name = name.?,
-            .complexity = complexity,
-        });
+fn add(
+    gpa: std.mem.Allocator,
+    arg_iterator: *std.process.Args.Iterator,
+    idea_list: *IdeaList,
+) !void {
+    const name = arg_iterator.next();
+    if (name == null) {
+        std.log.info("ERROR: missing required paramters: `sentinel add <name> <complexity>`", .{});
+        return;
+    }
+    const complexity_str = arg_iterator.next();
+    if (complexity_str == null) {
+        std.log.info("ERROR: missing required paramters: `sentinel add <name> <complexity>`", .{});
+        return;
     }
 
-    for (context_list.list.items) |it| {
-        std.log.debug("Name: {s}", .{it.name});
-        std.log.debug("Complexity: {d}", .{it.complexity});
+    const signal_str = arg_iterator.next();
+    if (signal_str == null) {
+        std.log.info("ERROR: missing required paramters: `sentinel add <name> <complexity>`", .{});
+        return;
+    }
+
+    const vision_str = arg_iterator.next();
+    if (vision_str == null) {
+        std.log.info("ERROR: missing required paramters: `sentinel add <name> <complexity>`", .{});
+        return;
+    }
+
+    const noise_str = arg_iterator.next();
+    if (noise_str == null) {
+        std.log.info("ERROR: missing required paramters: `sentinel add <name> <complexity>`", .{});
+        return;
+    }
+
+    const complexity = try std.fmt.parseInt(u4, complexity_str.?, 10);
+    const signal = try std.fmt.parseInt(u4, signal_str.?, 10);
+    const vision = try std.fmt.parseInt(u4, vision_str.?, 10);
+    const noise = try std.fmt.parseInt(u4, noise_str.?, 10);
+
+    if (idea_list.total_complexity + complexity > MAX_BIOLOGICAL_RAM) {
+        std.log.info("You're overloaded!", .{});
+        return;
+    }
+
+    try idea_list.add(gpa, .{
+        .name = name.?,
+        .complexity = complexity,
+        .signal = signal,
+        .vision = vision,
+        .noise = noise,
+    });
+}
+
+fn score(idea_list: IdeaList) void {
+    for (idea_list.list.items) |it| {
+        std.log.info("Name: {s}", .{it.name});
+        // Why these weights?
+        // Because Noise is a "Negative Gradient." It kills your productivity faster than Signal can
+        // build it.
+        const result =
+            @as(f32, @floatFromInt(it.signal)) * 0.5 +
+            @as(f32, @floatFromInt(it.vision)) * 0.4 -
+            @as(f32, @floatFromInt(it.noise)) * 0.8;
+        std.log.info("Score: {f}", .{result});
+    }
+}
+
+pub fn main(init: std.process.Init) !void {
+    var exit = false;
+    var idea_list: IdeaList = .{};
+    defer idea_list.deinit(init.gpa);
+
+    while (!exit) {
+        var arg_iterator = init.minimal.args.iterate();
+        defer arg_iterator.deinit();
+        _ = arg_iterator.next(); // skip the app bin path
+        const command: ?[:0]const u8 = arg_iterator.next();
+
+        // TODO: return error
+        if (command == null) return;
+
+        if (std.mem.eql(u8, command.?, "add")) {
+            try add(init.gpa, &arg_iterator, &idea_list);
+        } else if (std.mem.eql(u8, command.?, "score")) {
+            score(idea_list);
+        } else if (std.mem.eql(u8, command.?, "exit")) {
+            exit = true;
+        }
     }
 }
